2c8bd5d5f40fb522760f966e50dfb70076e8a9e5
Muhammad Nurfadhli <79645149+mozart-69@users.noreply.github.com>
2024-10-26 07:33:19 +0700

    Menambahkan fitur batalkan alarm

--- app/src/main/java/com/example/myalarmmanager/MainActivity.kt
+++ app/src/main/java/com/example/myalarmmanager/MainActivity.kt
@@ -50,6 +50,8 @@ class MainActivity : AppCompatActivity(),
         // Listener repeating alarm
         binding.btnRepeatingTime.setOnClickListener(this)
         binding.btnSetRepeatingAlarm.setOnClickListener(this)
+        binding.btnCancelRepeatingAlarm.setOnClickListener(this)
+
 
         alarmReceiver = AlarmReceiver()
     }
@@ -93,6 +95,9 @@ class MainActivity : AppCompatActivity(),
                     AlarmReceiver.TYPE_REPEATING,
                     repeatTime,
                     repeatMessage)
+            }
+            R.id.btn_cancel_repeating_alarm -> {
+                alarmReceiver.cancelAlarm(this, AlarmReceiver.TYPE_REPEATING)
             }
 
         }
